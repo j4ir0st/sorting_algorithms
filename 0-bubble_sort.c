18cc8beeab4ae74624d8b6d7d35da7f1e23e3412
j4ir0st <jairocast2@gmail.com>
2022-02-14 14:48:43 -0800

    Completing sort.h and modifying bubble sort function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j, act_v = 0;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
