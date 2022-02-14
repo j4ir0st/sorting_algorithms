c45453fa32fff3a6d7e6078d31740ab8f550ddcf
j4ir0st <jairocast2@gmail.com>
2022-02-14 14:20:51 -0800

    Deleting restiction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -26,9 +26,6 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	if (array == NULL)
-		printf("empy array");
-
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
