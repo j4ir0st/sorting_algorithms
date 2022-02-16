47c7a4dd766aea0dc84446cc1de3fcd55d812444
j4ir0st <jairocast2@gmail.com>
2022-02-16 12:58:43 -0500

    using slection sort function

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Entry point
+ * @array: pointer to the array
+ * @size: size of the array
+ * Return: Nothing
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, minor = 0;
+	int tmp = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		minor = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minor])
+			{
+				minor = j;
+			}
+		}
+		if (minor != i)
+		{
+			tmp = array[i];
+			array[i] = array[minor];
+			array[minor] = tmp;
+		}
+		print_array(array, size);
+	}
+}
